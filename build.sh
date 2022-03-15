e9ef9d2026dbe3c7201c2237507fb98908c80570
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:28:34 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-airrflow:2.0.0_0.1.0 -t truwl/nfcore-airrflow:latest .
+docker push truwl/nfcore-airrflow:2.0.0_0.1.0
+docker push truwl/nfcore-airrflow:latest
+        
